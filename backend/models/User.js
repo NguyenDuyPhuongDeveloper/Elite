e3a11ba7704fe2b91d349b0dcd865b08662663e9
NguyenDuyPhuongDeveloper <phuongnd0239@gmail.com>
2024-11-14 20:48:01 +0700

    Update User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,26 +1,49 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const UserProfileSchema = require('./UserProfile');
+const UserSubscriptionSchema = require('./UserSubscription');
+
 
 const UserSchema = new mongoose.Schema({
-    fullname: { type: String, required: true },
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    account_type: { type: String, enum: ['Basic', 'Premium', 'VIP'], default: 'Basic' },
-    created_at: { type: Date, default: Date.now },
-    profile: UserProfileSchema,
-    userSubscription: UserSubscriptionSchema,
-    events: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }],
-});
-UserProfileSchema.index({ location: '2dsphere' });
+    // Account Information
+    username: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, unique: true, required: true, lowercase: true },
+    password: { type: String, required: true, select: false, minlength: 8 },
+    phone: { type: String, unique: true, sparse: true },
 
-UserSchema.pre('save', async function (next)
-{
-    if (!this.isModified('password'))
-    {
-        next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-});
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    dateOfBirth: { type: Date, required: true },
+    gender: { type: String, enum: ['Male', 'Female', 'Other'] },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            default: 'Point'
+        },
+        coordinates: {
+            type: [Number],
+            index: '2dsphere'
+        },
+        city: { type: String },
+        country: { type: String }
+    },
+    // State
+    account_type: { type: String, enum: ['Basic', 'Premium', 'VIP'], default: 'Basic' },
+    account_status: { type: String, enum: ['Active', 'Inactive', 'Suspended'], default: 'Active' },
+
+    is_verified: { type: Boolean, default: false },
+    thirdPartyVerified: { type: Boolean, default: false },
+    // Security Settings
+    isTwoFactorAuthEnabled: {
+        type: Boolean,
+        default: false,
+    },
+    lastLogin: {
+        type: Date,
+    },
+
+    // userSubscription: { type: mongoose.Schema.Types.ObjectId, ref: 'UserSubscription' },
+    // events: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }],
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
